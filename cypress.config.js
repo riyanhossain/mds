301235e0161e537aef887ca2ade3a1619010a3ec
riyanhossain <mehraabriyan@gmail.com>
2025-08-02 19:32:45 +0600

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://mds-weld.vercel.app",
+    baseUrl: "http://localhost:4321",
     supportFile: "cypress/support/e2e.js",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
