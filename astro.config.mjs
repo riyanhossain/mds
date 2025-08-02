67fd56617b09772dd61fb5e9e72d484c4e73b994
riyanhossain <mehraabriyan@gmail.com>
2025-08-02 18:46:08 +0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
-import { loadEnv } from "vite";
 
 import vercel from "@astrojs/vercel";
-
-const env = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
 
 export default defineConfig({
   integrations: [
     sanity({
-      projectId: env.PUBLIC_SANITY_PROJECT_ID ?? "dzwl1s75",
-      dataset: env.PUBLIC_SANITY_DATASET ?? "production",
+      projectId: "dzwl1s75",
+      dataset: "production",
       useCdn: false,
       studioBasePath: "/studio",
     }),
