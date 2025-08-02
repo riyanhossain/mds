fa78744fbbc3b9fdc5cdfcd55bc8a6c8856d5ff0
riyanhossain <mehraabriyan@gmail.com>
2025-08-02 17:58:04 +0600

    wip

---
+++ cypress/e2e/visual.cy.js
@@ -0,0 +1,12 @@
+describe("Visual regression tests", function () {
+  it("Loads the homepage and takes Percy snapshot", function () {
+    // Load the homepage
+    cy.visit("/");
+
+    // Wait for the page to be fully loaded
+    cy.get("body").should("be.visible");
+
+    // Take a snapshot for visual diffing
+    cy.percySnapshot("Homepage");
+  });
+});
